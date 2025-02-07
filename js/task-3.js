2c47f987f583b17680400bd381948e1b16a2873a
Pereverza Tetiana <tanyusha.vzletka82@gmail.com>
2025-02-07 11:25:36 +0200

    commit messages

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,9 @@
 function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding) * 2;
+    const borderWidth = parseFloat(border) * 2;
+     return contentWidth + paddingWidth + borderWidth;
     
-    return content + padding + border
-   
 
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
